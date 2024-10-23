924767549c55878c5d1bd64ab70936e513de1c57
Icarus <871227769@qq.com>
2024-10-24 00:43:22 +0800

    先全部清空

--- tmp_out_file.py
+++ tmp_out_file.py
@@ -2,7 +2,4 @@ from pathlib import Path
 
 
 if __name__ == '__main__':
-    print('this is out file.')
-    print('第一次的commit...')
-    print('第二次的commit提交...')
-    print('第三次的commit提交...')
+    print('我先全删了')
