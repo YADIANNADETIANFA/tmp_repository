c5091f96c0162f8922eee2d445a78fef12ed20c0
Icarus <871227769@qq.com>
2024-10-28 23:27:08 +0800

    再次全部清空，初始化

--- tmp_out_file.py
+++ tmp_out_file.py
@@ -2,7 +2,4 @@ from pathlib import Path
 
 
 if __name__ == '__main__':
-    print('2024-10-28，全部清空')
-    print('基座的一行...，在github上直接进行修改')
-
-    print('基座的一行...在本地修改基座')
+    print('2024-10-28，全部清空_2')
