4c52dd91144ca4104f83ef6d94f05ec171c20690
Icarus <871227769@qq.com>
2024-10-29 22:50:10 +0800

    全部清空，准备学习tags和releases

--- tmp_out_file.py
+++ tmp_out_file.py
@@ -2,7 +2,4 @@ from pathlib import Path
 
 
 if __name__ == '__main__':
-    print('2024-10-28，全部清空_3')
-    print('基座3......github上进行修改')
-
-    print('基座3......本地进行修改')
+    print('2024-10-29，全部清空，用于tag和release的学习')
