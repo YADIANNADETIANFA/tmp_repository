53e1742ee409af2e81cd03fb1618b0013e733127
Icarus <871227769@qq.com>
2024-10-31 23:20:56 +0800

    初始化，准备学习PR

--- tmp_out_file.py
+++ tmp_out_file.py
@@ -2,6 +2,4 @@ from pathlib import Path
 
 
 if __name__ == '__main__':
-    print('2024-10-29，全部清空，用于tag和release的学习')
-    print('创建一行代码，并在这里创建tag v1.0.0')
-    print('创建第二行代码，在这里创建tag v1.0.1，并基于此标签进行release发布')
+    print('2024.10.31，全部清空，准备学习PR')
