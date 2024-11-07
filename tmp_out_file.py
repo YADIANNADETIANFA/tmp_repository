363713ba90a3850ce1b525b73bec0f6292b108a8
Icarus <871227769@qq.com>
2024-11-07 23:45:15 +0800

    准备学习PR，现在初始化代码工程

--- tmp_out_file.py
+++ tmp_out_file.py
@@ -2,6 +2,4 @@ from pathlib import Path
 
 
 if __name__ == '__main__':
-    print('2024.10.31，全部清空，准备学习PR。。。zk分支，在这里进行的修改')
-    print('zk分支，加了这里一行')
-    print('zk分支，加了第二行')
+    print('2024.11.7，全部初始化，准备学习PR')
