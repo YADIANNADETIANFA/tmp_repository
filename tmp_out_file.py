73cadcc741607184eca30fb59f5d4f81e431a4b7
Icarus <871227769@qq.com>
2024-11-09 00:42:38 +0800

    全部初始化，通过merge，再试一次PR

--- tmp_out_file.py
+++ tmp_out_file.py
@@ -2,6 +2,4 @@ from pathlib import Path
 
 
 if __name__ == '__main__':
-    print('2024.11.7，全部初始化，准备学习PR')
-    print('main分支自身，也进行了提交处理，就是为了显式冲突')
-    print('从main分支new出来的another分支，在这里进行了提交时')
+    print('2024.11.9，全部初始化，准备学习PR')
